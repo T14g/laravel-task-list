02f8aa07a1ae21eb928bba175bbe54c362096074
T14g <ottiagos@gmail.com>
2026-07-22 18:11:56 -0300

    Making some routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return "Hello world!";
 });
+
+
+Route::get('/about', function () {
+    return "About page";
+});
+
+Route::get('/hello', function () {
+    return "Hello page";
+})->name('hello');
+
+Route::get('/hi', function () {
+    return redirect()->route('hello');
+});
+
+Route::fallback(function () {
+    return "404 page not found";
+});
+
+Route::get('/users/{id}', function ($id) {
+    return "User ID: " . $id;
+});
