d08bade1cd0ced6c8f7ab158ee2e588fbe4dc8e6
T14g <ottiagos@gmail.com>
2026-07-30 19:33:08 -0300

    tasks class and mock data showing on home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,15 @@
-<h1>Hello world {{ $name }}</h1>
+<h1>
+  The list of tasks
+</h1>
+
+<div>
+  {{-- @if (count($tasks)) --}}
+  @forelse ($tasks as $task)
+    <div>
+      <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+    </div>
+  @empty
+    <div>There are no tasks!</div>
+  @endforelse
+  {{-- @endif --}}
+</div>
